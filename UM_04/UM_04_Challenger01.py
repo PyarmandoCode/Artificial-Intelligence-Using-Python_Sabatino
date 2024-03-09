9cca11f81b1c5dbbd9504cdbe2d2cb09572da983
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2024-03-09 14:18:49 -0500

    Clase04

--- UM_04/UM_04_Challenger01.py
+++ UM_04/UM_04_Challenger01.py
@@ -16,4 +16,66 @@ Requisitos Adicionales:
 -Debes manejar situaciones donde el usuario intenta agregar una cantidad mayor que la disponible en el stock.
 -El programa debe proporcionar retroalimentación adecuada al usuario en cada operación realizada.(usar excepciones)
 """
+import csv
 
+def cargar_productos():
+    try:
+        with open('./UM_04/archivos/productos.csv','r') as file:
+            reader = csv.DictReader(file)  
+            productos=list(reader)
+            return productos
+    except Exception as err:
+        return f"Ocurrio un Error {err}"    
+    
+def mostrar_productos(productos):
+    print("Productos Disponibles")
+    for producto in productos:
+        print(f"{producto['ID']}: {producto['NOMBRE']} - ${str(producto['PRECIO'])} - {producto['STOCK']}")
+
+def agregar_carrito(carrito,productos,producto_id,cantidad):
+    
+    for producto in productos:
+        if producto["ID"]==producto_id:
+            stock_disponible=int(producto['STOCK'])
+            if cantidad<=stock_disponible:
+                carrito.append({
+                    'ID':producto['ID'],
+                    'NOMBRE':producto['NOMBRE'],
+                    'PRECIO':float(producto['PRECIO']),
+                    'CANTIDAD':cantidad
+                })
+                producto['STOCK']=str(stock_disponible-cantidad)
+                print(f"Producto Agregado al carrito {producto['NOMBRE']} X {cantidad}")
+            else:
+                print("No Hay Stock Suficiente")        
+            return
+    print("Producto No Disponible")    
+              
+def ver_carrito(carrito):
+    total=sum(item['PRECIO'] * item['CANTIDAD']for item in carrito)
+    print("\nContenido del Carrito")
+    for item in carrito:
+        print(f"{item['NOMBRE']} x {item['CANTIDAD']} - ${item['PRECIO']*item['CANTIDAD']:.2f}")
+    print(f"\nTotal a Pagar ${total:.2f}\n")    
+
+def main():
+    productos=cargar_productos()
+    carrito=[]
+    while True:
+        mostrar_productos(productos)
+        opcion=input("\n1.-Agregar Al Carrito\n2.-Ver Carrito\n3.-Realizar Pago\n4.-Salir\n")
+        if opcion =='1':
+            producto_id=input("Ingrese el codigo a comprar:")
+            cantidad=int(input("Ingrese la cantidad:"))
+            agregar_carrito(carrito,productos,producto_id,cantidad)
+        elif opcion=='2':
+            ver_carrito(carrito)    
+        elif opcion =='3':
+            print("Realizar Pago")    
+        elif opcion=='4':
+            break
+        else:
+            print("Opcion No valida Intente de nuevo")
+
+if __name__ =='__main__':
+    main()
